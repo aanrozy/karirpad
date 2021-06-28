d3806941440a7a4820ac836f4158816528fa6e2e
aanrozy <30997157+aanrozy@users.noreply.github.com>
2021-06-28 14:24:56 +0700

    sql added

--- karirpad.sql
+++ karirpad.sql
@@ -12,15 +12,93 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+-- Dumping structure for table karirpad.barang
+CREATE TABLE IF NOT EXISTS `barang` (
+  `barang_id` int(11) NOT NULL AUTO_INCREMENT,
+  `foto_barang` mediumblob NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `kategori` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  PRIMARY KEY (`barang_id`) USING BTREE,
+  UNIQUE KEY `nama_barang` (`nama_barang`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table karirpad.barang: ~4 rows (approximately)
+/*!40000 ALTER TABLE `barang` DISABLE KEYS */;
+INSERT INTO `barang` (`barang_id`, `foto_barang`, `nama_barang`, `kategori`, `harga`) VALUES
+	(12, _binary 0x312E706E67, 'nama1', 'kategori1', 51000),
+	(13, _binary 0x322E706E67, 'nama2', 'kategori2', 45000),
+	(14, _binary 0x332E706E67, 'nama3', 'kategori3', 2000),
+	(16, _binary 0x53637265656E73686F74202832292E706E67, 'barang4', 'barang', 25000);
+/*!40000 ALTER TABLE `barang` ENABLE KEYS */;
 
--- Data exporting was unselected.
+-- Dumping structure for table karirpad.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table karirpad.failed_jobs: ~0 rows (approximately)
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
 
--- Data exporting was unselected.
+-- Dumping structure for table karirpad.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table karirpad.migrations: ~5 rows (approximately)
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2014_10_12_100000_create_password_resets_table', 1),
+	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(5, '2021_06_28_070211_last_login_at', 3);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+
+-- Dumping structure for table karirpad.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table karirpad.password_resets: ~0 rows (approximately)
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+
+-- Dumping structure for table karirpad.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `last_login_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table karirpad.users: ~2 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `last_login_at`) VALUES
+	(1, 'yourname7', 'yourname7xx@gmail.com', NULL, '$2y$10$qYHYAHWVeX6WlT8A1HCzB.F2.Kecvj7n0SsZE1oZ1DcxvyRYf20iW', NULL, '2021-06-25 21:35:46', '2021-06-28 07:18:17', '2021-06-28 07:18:17'),
+	(2, 'admin', 'admin@mail.com', NULL, '$2y$10$nsXLUCzrm4RckE6gi1DcteYvbuakNeXmfAeUjTlgHsB/QQY3fr4NG', NULL, '2021-06-26 04:49:01', '2021-06-28 07:10:34', '2021-06-28 07:10:34');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
